38b575ce0ac98bddcfb81b37408736a9393a738a
solomonmbotiah <99472574+Tsumahhh@users.noreply.github.com>
2025-03-07 20:53:13 +0300

    Tsuma-PB

--- settings.js
+++ settings.js
@@ -2,34 +2,34 @@ const fs = require('fs')
 const chalk = require('chalk')
 //Feel free to customize my bot informations to yours. Starting from this point
 //owmner v card
-global.ytname = "YT: ElitePro" //ur yt chanel name
-global.socialm = "GitHub: Elite-MD" //ur github or insta name
-global.location = "Port Harcourt, Nigeria" //ur location
+global.ytname = "YT: Tsuma-PB" //ur yt chanel name
+global.socialm = "GitHub: Tsumahhh" //ur github or insta name
+global.location = "Nairobi, Kenya" //ur location
 
 //new settings 
-global.botname = 'ELITE-PRO-V2' //ur bot name
-global.ownernumber = '2348109263390' //ur owner number
-global.ownername = 'ELITE-PRO-V2' //ur owner name
+global.botname = 'Tsuma-PB' //ur bot name
+global.ownernumber = '254743399594' //ur owner number
+global.ownername = 'TSUMA' //ur owner name
 global.websitex = "https://youtu.be/@elitepro-empire"
-global.elitepropp = 'https://i.ibb.co/jk0ynvbn/7a8c4b5b617fa11a1e9a61190f427546.jpg' // Change this image link to yours.
+global.elitepropp = 'https:https://imgur.com/3uJbbXW' // Change this image link to yours.
 
 global.themeemoji = '👨🏼‍💻'
-global.wm = "ELITE-PRO-V2"
+global.wm = "Tsuma-PB"
 global.botscript = 'https://github.com/Elite-MD/Elite-Pro-V2' //script link
 global.packname = "Sticker By"
-global.author = "ELITE-PRO-V2\n\n+2348109263390"
-global.creator = "2348109263390@s.whatsapp.net"
+global.author = "TSUMA\n\n+2348109263390"
+global.creator = "254743399594@s.whatsapp.net"
 global.xprefix = '.' //set any prefix 
-global.premium = ["2348109263390"] // Premium User
-//Stop here. Thanks for choosing ELITEPRO.
+global.premium = ["254743399594"] // Premium User
+//Stop here. Thanks for choosing TSUMA-PB.
 
 
 //bot settings 
 global.hituet = 0
 global.typemenu = 'v1'
 global.typereply = 'v4' 
-global.autoblocknumber = '92' //set autoblock country code
-global.antiforeignnumber = '91' //set anti foreign number country code
+global.autoblocknumber = '' //set autoblock country code
+global.antiforeignnumber = '' //set anti foreign number country code
 global.welcome = false //welcome/left in groups
 global.anticall = false //bot blocks user when called
 global.autoswview = false //auto status/story view
@@ -48,7 +48,7 @@ global.mess = {
 
 //text bug
 global.xbugtex = {
-xtxt: '👨🏼‍💻ELITE-PRO-V2👨🏼‍💻',
+xtxt: '👨🏼‍💻TSUMA-PB👨🏼‍💻',
 }
 
 
